bb6fd38c28ed8cd32cd7e425b681319e7b8633b0
Zubair Jamil <xubairjamil@gmail.com>
2025-01-02 08:44:04 +0500

    Add EF Core for the scheme management

--- MarkMe.Database/Entities/Department.cs
+++ MarkMe.Database/Entities/Department.cs
@@ -1,8 +1,12 @@
-﻿namespace MarkMe.Database.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MarkMe.Database.Entities
 {
     public class Department
     {
         public int DepartmentId { get; set; }
+
+        [StringLength(50)]
         public string Name { get; set; } = string.Empty;
         public bool IsDeleted { get; set; }
     }
---
+++ MarkMe.Database/Migrations/20250102034147_Init.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MarkMe.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Attendances",
+                columns: table => new
+                {
+                    AttendanceId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentId = table.Column<int>(type: "int", nullable: false),
+                    CourseId = table.Column<int>(type: "int", nullable: false),
+                    DateMarked = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    MarkedBy = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Attendances", x => x.AttendanceId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ClassRepresentatives",
+                columns: table => new
+                {
+                    StudentId = table.Column<int>(type: "int", nullable: false),
+                    CourseId = table.Column<int>(type: "int", nullable: false),
+                    IsDeleted = table.Column<int>(type: "int", nullable: false),
+                    NominatedBy = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClassRepresentatives", x => new { x.StudentId, x.CourseId });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    CourseId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Code = table.Column<string>(type: "nvarchar(6)", maxLength: 6, nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    Semester = table.Column<int>(type: "int", maxLength: 50, nullable: false),
+                    CreditHours = table.Column<int>(type: "int", nullable: false),
+                    CreditPerHoursWeek = table.Column<int>(type: "int", nullable: false),
+                    IsArchived = table.Column<bool>(type: "bit", nullable: false),
+                    AssignedTo = table.Column<int>(type: "int", nullable: false),
+                    TeacherId = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.CourseId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new
+                {
+                    DepartmentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departments", x => x.DepartmentId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Enrolments",
+                columns: table => new
+                {
+                    EnrolmentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentId = table.Column<int>(type: "int", nullable: false),
+                    CourseId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Enrolments", x => x.EnrolmentId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    StudentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CollegeRollNo = table.Column<string>(type: "nvarchar(4)", maxLength: 4, nullable: false),
+                    UniversityRollNo = table.Column<string>(type: "nvarchar(6)", maxLength: 6, nullable: false),
+                    RegistrationNo = table.Column<string>(type: "nvarchar(12)", maxLength: 12, nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Session = table.Column<string>(type: "nvarchar(9)", maxLength: 9, nullable: false),
+                    Section = table.Column<string>(type: "nvarchar(9)", maxLength: 9, nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.StudentId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "StudentId", "CollegeRollNo", "FirstName", "IsDeleted", "LastName", "RegistrationNo", "Section", "Session", "UniversityRollNo" },
+                values: new object[,]
+                {
+                    { 1, "537", "Zubair", false, "Jamil", "2021-gsr-439", "G1", "2021-2025", "070941" },
+                    { 2, "539", "Umair", false, "Jamil", "2021-gsr-442", "G1", "2021-2025", "070943" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "UserId", "Email", "FirstName", "IsDeleted", "LastName", "Password" },
+                values: new object[] { 1, "xubairjamil@gmail.com", "Zubair", false, "Jamil", "123456" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Attendances");
+
+            migrationBuilder.DropTable(
+                name: "ClassRepresentatives");
+
+            migrationBuilder.DropTable(
+                name: "Courses");
+
+            migrationBuilder.DropTable(
+                name: "Departments");
+
+            migrationBuilder.DropTable(
+                name: "Enrolments");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
