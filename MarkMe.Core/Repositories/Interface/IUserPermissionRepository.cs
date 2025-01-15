0c7987cba1c071242533165689acef4956ffe88e
Zubair Jamil <xubairjamil@gmail.com>
2025-01-15 21:48:07 +0500

    WIP

---
+++ MarkMe.Core/Repositories/Interface/IUserPermissionRepository.cs
@@ -0,0 +1,9 @@
+﻿using MarkMe.Database.Entities;
+
+namespace MarkMe.Core.Repositories.Interface
+{
+    public interface IUserPermissionRepository
+    {
+        Task<IEnumerable<Menu>> GetAllAsync();
+    }
+}
---
+++ MarkMe.Core/Repositories/UserPermissionRepository.cs
@@ -0,0 +1,19 @@
+﻿using MarkMe.Core.Repositories.Interface;
+using MarkMe.Database.Entities;
+using MarkMe.Database.Interface;
+
+namespace MarkMe.Core.Repositories
+{
+    public class UserPermissionRepository(IDatabase _database) : IUserPermissionRepository
+    {
+        public async Task<IEnumerable<Menu>> GetAllAsync()
+        {
+            const string sql = """ 
+					SELECT * 
+					FROM Menus
+					""";
+            var res = await _database.QueryAsync<Menu>(sql);
+            return res;
+        }
+    }
+}
